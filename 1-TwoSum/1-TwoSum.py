b43d5a19c8e0075bd83327958b9e13a8d6067bdc
Andrea Garza <80288669+andreaagarzaa@users.noreply.github.com>
2025-02-11 09:39:07 -0600

    File updated at 11/2/2025, 9:39:07 a.m.

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -5,19 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        dic = {}  # Diccionario para almacenar números e índices
-
-        for index, num in enumerate(nums):
-            # Calcular lo necesario para llegar al target
-            complement = target - num
-
-            # Verificar si el complemento existe en el diccionario
-            if complement in dic:
-                return [dic[complement], index]
-
-            # Almacenar el número con su índice
-            dic[num] = index
-
-        # Retorna una lista vacía si no se encuentra una solución (aunque el problema garantiza que hay una)
+        # Recorrer todos los pares posibles
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         return []
+        
 

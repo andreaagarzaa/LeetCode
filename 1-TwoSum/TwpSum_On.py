391d82867774daa1b2a011af88999c0a655b3d1a
Andrea Garza <80288669+andreaagarzaa@users.noreply.github.com>
2025-02-11 09:43:50 -0600

    Add files via upload

---
+++ 1-TwoSum/TwpSum_On.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}  # Diccionario para almacenar números e índices
+
+        for index, num in enumerate(nums):
+            # Calcular lo necesario para llegar al target
+            complement = target - num
+
+            # Verificar si el complemento existe en el diccionario
+            if complement in dic:
+                return [dic[complement], index]
+
+            # Almacenar el número con su índice
+            dic[num] = index
+
+        # Retorna una lista vacía si no se encuentra una solución (aunque el problema garantiza que hay una)
+        return []
